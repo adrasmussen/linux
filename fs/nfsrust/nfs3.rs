45018fa38541a13aaf2b9ec535804c75e7eca626
tyler port <tyler274port@gmail.com>
2023-12-29 15:21:40 +0100

    make the constant usable in the module

--- fs/nfsrust/nfs3.rs
+++ fs/nfsrust/nfs3.rs
@@ -39,54 +39,54 @@ impl Drop for RustMinimal {
     }
 }
 
-const PORT: i32 = 2049;
-const MAXDATA: i32 = 32768;
-const MAXPATHLEN: u32 = PATH_MAX; 
-const MAXNAMLEN: u32 = NAME_MAX;
-const MAXGROUPS: i32 = 16;
-const FHSIZE: i32 = 64;
-const COOKIESIZE: i32 = 4;
-const CREATEVERFSIZE: i32 = 8;
-const COOKIEVERFSIZE: i32 = 8;
-const WRITEVERFSIZE: i32 = 8;
-const FIFO_DEV: i32 = -1;
-const NFS3MODE_FMT: i32 = 0170000;
-const NFS3MODE_DIR: i32 = 0040000;
-const NFS3MODE_CHR: i32 = 0020000;
-const NFS3MODE_BLK: i32 = 0060000;
-const NFS3MODE_REG: i32 = 0100000;
-const NFS3MODE_LNK: i32 = 0120000;
-const NFS3MODE_SOCK: i32 = 0140000;
-const NFS3MODE_FIFO: i32 = 0010000;
+pub(crate) const PORT: i32 = 2049;
+pub(crate) const MAXDATA: i32 = 32768;
+pub(crate) const MAXPATHLEN: u32 = PATH_MAX; 
+pub(crate) const MAXNAMLEN: u32 = NAME_MAX;
+pub(crate) const MAXGROUPS: i32 = 16;
+pub(crate) const FHSIZE: i32 = 64;
+pub(crate) const COOKIESIZE: i32 = 4;
+pub(crate) const CREATEVERFSIZE: i32 = 8;
+pub(crate) const COOKIEVERFSIZE: i32 = 8;
+pub(crate) const WRITEVERFSIZE: i32 = 8;
+pub(crate) const FIFO_DEV: i32 = -1;
+pub(crate) const NFS3MODE_FMT: i32 = 0170000;
+pub(crate) const NFS3MODE_DIR: i32 = 0040000;
+pub(crate) const NFS3MODE_CHR: i32 = 0020000;
+pub(crate) const NFS3MODE_BLK: i32 = 0060000;
+pub(crate) const NFS3MODE_REG: i32 = 0100000;
+pub(crate) const NFS3MODE_LNK: i32 = 0120000;
+pub(crate) const NFS3MODE_SOCK: i32 = 0140000;
+pub(crate) const NFS3MODE_FIFO: i32 = 0010000;
 
 /* Flags for access() call */
-const ACCESS_READ: i32 = 0x0001;
-const ACCESS_LOOKUP: i32 = 0x0002;
-const ACCESS_MODIFY: i32 = 0x0004;
-const ACCESS_EXTEND: i32 = 0x0008;
-const ACCESS_DELETE: i32 = 0x0010;
-const ACCESS_EXECUTE: i32 = 0x0020;
-const ACCESS_FULL: i32 = 0x003f;
+pub(crate) const ACCESS_READ: i32 = 0x0001;
+pub(crate) const ACCESS_LOOKUP: i32 = 0x0002;
+pub(crate) const ACCESS_MODIFY: i32 = 0x0004;
+pub(crate) const ACCESS_EXTEND: i32 = 0x0008;
+pub(crate) const ACCESS_DELETE: i32 = 0x0010;
+pub(crate) const ACCESS_EXECUTE: i32 = 0x0020;
+pub(crate) const ACCESS_FULL: i32 = 0x003f;
 
 /* Flags for create mode */
-enum CreateMode {
+pub(crate) enum CreateMode {
     UNCHECKED = 0,
     GUARDED = 1,
     EXCLUSIVE = 2,
 }
 
 /* NFSv3 file system properties */
-const FSF_LINK: i32 = 0x0001;
-const FSF_SYMLINK: i32 = 0x0002;
-const FSF_HOMOGENEOUS: i32 = 0x0008;
-const FSF_CANSETTIME: i32 = 0x0010;
+pub(crate) const FSF_LINK: i32 = 0x0001;
+pub(crate) const FSF_SYMLINK: i32 = 0x0002;
+pub(crate) const FSF_HOMOGENEOUS: i32 = 0x0008;
+pub(crate) const FSF_CANSETTIME: i32 = 0x0010;
 
 /* Some shorthands. See fs/nfsd/nfs3proc.c */
-const FSF_DEFAULT: i32 = 0x001B;
-const FSF_BILLYBOY: i32 = 0x0018;
-const FSF_READONLY: i32 = 0x0008;
+pub(crate) const FSF_DEFAULT: i32 = 0x001B;
+pub(crate) const FSF_BILLYBOY: i32 = 0x0018;
+pub(crate) const FSF_READONLY: i32 = 0x0008;
 
-enum FType {
+pub(crate) enum FType {
     NON = 0,
     REG = 1,
     DIR = 2,
@@ -98,39 +98,39 @@ enum FType {
     BAD  = 8,
 }
 
-enum TimeHow {
+pub(crate) enum TimeHow {
     DONT_CHANGE = 0,
     SET_TO_SERVER_TIME = 1,
     SET_TO_CLIENT_TIME = 2,
 }
 
-struct FH {
+pub(crate) struct FH {
     size: u16,
     data: [u8; FHSIZE as usize],
 }
 
-const VERSION: i32 = 3;
-const PROC_NULL: i32 = 0;
-const PROC_GETATTR: i32 = 1;
-const PROC_SETATTR: i32 = 2;
-const PROC_LOOKUP: i32 = 3;
-const PROC_ACCESS: i32 = 4;
-const PROC_READLINK: i32 = 5;
-const PROC_READ: i32 = 6;
-const PROC_WRITE: i32 = 7;
-const PROC_CREATE: i32 = 8;
-const PROC_MKDIR: i32 = 9;
-const PROC_SYMLINK: i32 = 10;
-const PROC_MKNOD: i32 = 11;
-const PROC_REMOVE: i32 = 12;
-const PROC_RMDIR: i32 = 13;
-const PROC_RENAME: i32 = 14;
-const PROC_LINK: i32 = 15;
-const PROC_READDIR: i32 = 16;
-const PROC_READDIRPLUS: i32 = 17;
-const PROC_FSSTAT: i32 = 18;
-const PROC_FSINFO: i32 = 19;
-const PROC_PATHCONF: i32 = 20;
-const PROC_COMMIT: i32 = 21;
+pub(crate) const VERSION: i32 = 3;
+pub(crate) const PROC_NULL: i32 = 0;
+pub(crate) const PROC_GETATTR: i32 = 1;
+pub(crate) const PROC_SETATTR: i32 = 2;
+pub(crate) const PROC_LOOKUP: i32 = 3;
+pub(crate) const PROC_ACCESS: i32 = 4;
+pub(crate) const PROC_READLINK: i32 = 5;
+pub(crate) const PROC_READ: i32 = 6;
+pub(crate) const PROC_WRITE: i32 = 7;
+pub(crate) const PROC_CREATE: i32 = 8;
+pub(crate) const PROC_MKDIR: i32 = 9;
+pub(crate) const PROC_SYMLINK: i32 = 10;
+pub(crate) const PROC_MKNOD: i32 = 11;
+pub(crate) const PROC_REMOVE: i32 = 12;
+pub(crate) const PROC_RMDIR: i32 = 13;
+pub(crate) const PROC_RENAME: i32 = 14;
+pub(crate) const PROC_LINK: i32 = 15;
+pub(crate) const PROC_READDIR: i32 = 16;
+pub(crate) const PROC_READDIRPLUS: i32 = 17;
+pub(crate) const PROC_FSSTAT: i32 = 18;
+pub(crate) const PROC_FSINFO: i32 = 19;
+pub(crate) const PROC_PATHCONF: i32 = 20;
+pub(crate) const PROC_COMMIT: i32 = 21;
 
-const MNT_VERSION: i32 = 3;
+pub(crate) const MNT_VERSION: i32 = 3;
--- fs/nfsrust/nfsd.rs
+++ fs/nfsrust/nfsd.rs
@@ -4,3 +4,9 @@
 //! ./tools/testing/kunit/kunit.py run --make_options LLVM=1 --arch x86_64 --kconfig_add CONFIG_RUST=y --kconfig_add CONFIG_NFS_RUST=y --kconfig_add CONFIG_NFS_SAMPLE_RUST_MINIMAL=y
 
 use kernel::prelude::*;
+
+// use super::nfs3;
+
+mod nfs3;
+
+const a: i32 = nfs3::PORT;
