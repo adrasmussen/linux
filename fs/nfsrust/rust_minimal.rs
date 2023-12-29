c7e46c8ba8f520dd8df6c819f493ad8d5100364f
tyler port <tyler274port@gmail.com>
2023-12-29 11:00:57 +0100

    tests!

--- fs/nfsrust/rust_minimal.rs
+++ fs/nfsrust/rust_minimal.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 
 //! Rust minimal sample.
+//! Run tests with this:
+//! ./tools/testing/kunit/kunit.py run --make_options LLVM=1 --arch x86_64 --kconfig_add CONFIG_RUST=y --kconfig_add CONFIG_NFS_RUST=y --kconfig_add CONFIG_NFS_SAMPLE_RUST_MINIMAL=y
 
 use kernel::prelude::*;
 
